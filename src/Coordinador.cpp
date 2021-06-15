ae622ce0d63a9f3224d4ae00509048445f068d9e
pedro.menchero.meco@alumnos.upm.es <pedro.menchero.meco@alumnos.upm.es>
2021-06-15 22:22:45 +0200

    pantalla inicio final

--- src/Coordinador.cpp
+++ src/Coordinador.cpp
@@ -6,13 +6,40 @@ void Coordinador::dibuja()
 		gluLookAt(0, 7.5, 30, // posicion del ojo
 			0.0, 7.5, 0.0, // hacia que punto mira (0,7.5,0)
 			0.0, 1.0, 0.0); // definimos hacia arriba (eje Y)
+		glEnable(GL_TEXTURE_2D);
+
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/galaxia_victoria.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+
+		glTexCoord2d(0, 1);		glVertex3f(-20, 0, -0.1);
+		glTexCoord2d(1, 1);		glVertex3f(20, 0, -0.1);
+		glTexCoord2d(1, 0);		glVertex3f(20, 15, -0.1);
+		glTexCoord2d(0, 0);		glVertex3f(-20, 15, -0.1);
+		glEnd();
+
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
 		ETSIDI::setTextColor(1, 1, 0);
-		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
-		ETSIDI::printxy("SPACE Y", -5, 8);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 45);
+		ETSIDI::printxy("SPACE Y", -5, 12);
+
 		ETSIDI::setTextColor(1, 1, 1);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
-		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -5, 7);
-		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 6);
+		ETSIDI::printxy("PULSE -E- PARA EMPEZAR", -5, 9);
+		ETSIDI::printxy("PULSE -S- PARA SALIR", -5, 8);
+
+		ETSIDI::setTextColor(0, 1, 1);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 15);
+		ETSIDI::printxy("CONTROLES:", -5, 6);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 10);
+		ETSIDI::printxy("DISPARO: ESPACIO", -5, 5);
+		ETSIDI::printxy("DISPARO ESPECIAL: S", -5, 4);
+		ETSIDI::printxy("MOVIMIENTO ASCENDENTE: CURSOR ARRIBA", -5, 3);
+		ETSIDI::printxy("MOVIMIENTO DESCENDENTE: CURSOR ABAJO", -5, 2);
+		//
+
 	}
 	else if (estado == JUEGO)
 	{
@@ -28,15 +55,56 @@ void Coordinador::dibuja()
 	}
 	else if (estado == GAMEOVER)
 	{
-		mundo.dibuja();
+		//mundo.dibuja();
+
+		gluLookAt(0, 7.5, 30, // posicion del ojo
+			0.0, 7.5, 0.0, // hacia que punto mira (0,7.5,0)
+			0.0, 1.0, 0.0); // definimos hacia arriba (eje Y)
+		glEnable(GL_TEXTURE_2D);
+
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo_1.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+
+		glTexCoord2d(0, 1);		glVertex3f(-20, 0, -0.1);
+		glTexCoord2d(1, 1);		glVertex3f(20, 0, -0.1);
+		glTexCoord2d(1, 0);		glVertex3f(20, 15, -0.1);
+		glTexCoord2d(0, 0);		glVertex3f(-20, 15, -0.1);
+		glEnd();
+
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
 		ETSIDI::setTextColor(1, 0, 0);
-		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
-		ETSIDI::printxy("GAMEOVER: Has perdido", -5, 10);
-		ETSIDI::printxy("Pulsa -C- para continuar", -5, 5);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 35);
+		ETSIDI::printxy("GAMEOVER", -7, 8);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+		ETSIDI::printxy("TU NAVE HA SIDO DERROTADA", -10, 6);
+		ETSIDI::setTextColor(1, 1, 1);
+		ETSIDI::setFont("fuentes/Bitwise.ttf", 15);
+		ETSIDI::printxy("Pulsa -C- para continuar", -5, 3);
 	}
 	else if (estado == FIN)
 	{
-		mundo.dibuja();
+		//mundo.dibuja();
+		gluLookAt(0, 7.5, 30, // posicion del ojo
+			0.0, 7.5, 0.0, // hacia que punto mira (0,7.5,0)
+			0.0, 1.0, 0.0); // definimos hacia arriba (eje Y)
+		glEnable(GL_TEXTURE_2D);
+
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo_victoria.jpeg").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+
+		glTexCoord2d(0, 1);		glVertex3f(-20, 0, -0.1);
+		glTexCoord2d(1, 1);		glVertex3f(20, 0, -0.1);
+		glTexCoord2d(1, 0);		glVertex3f(20, 15, -0.1);
+		glTexCoord2d(0, 0);		glVertex3f(-20, 15, -0.1);
+		glEnd();
+
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
 		ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
 		ETSIDI::printxy("ENHORABUENA, ¡Has triunfado!", -5, 10);
 		ETSIDI::printxy("Pulsa -C- para continuar", -5, 9);
@@ -47,19 +115,22 @@ void Coordinador::dibuja()
 Coordinador::Coordinador()
 {
 	estado = INICIO;
+	ETSIDI::playMusica("sonidos/resumen_2.mp3", true);
 }
 void Coordinador::tecla(unsigned char key)
 {
 
 	if (estado == INICIO)
 	{
+
 		if (key == 'e')
 		{
 			ETSIDI::play("sonidos/morse.wav");
+
 			mundo.~Mundo();
 			mundo.inicializa();
 			estado = JUEGO;
-			ETSIDI::playMusica("sonidos/resumen_2.mp3", true);
+			ETSIDI::stopMusica();
 		}
 
 		if (key == 's')
@@ -74,11 +145,13 @@ void Coordinador::tecla(unsigned char key)
 	}
 	else if (estado == GAMEOVER)
 	{
+
 		if (key == 'c')
 			estado = INICIO;
 	}
 	else if (estado == FIN)
 	{
+
 		if (key == 'c')
 			estado = INICIO;
 	}
@@ -86,7 +159,7 @@ void Coordinador::tecla(unsigned char key)
 	{
 		if (key == 'c')
 			estado = JUEGO;
-		ETSIDI::playMusica("sonidos/resumen_2.mp3", true);
+
 	}
 }
 void Coordinador::teclaEspecial(unsigned char key)
@@ -98,15 +171,22 @@ void Coordinador::mueve()
 {
 	if (estado == JUEGO)
 	{
+
 		mundo.mueve();
+
 		if (mundo.getNumObstaculos() == 0)
 		{
-		
+
 			if (!mundo.cargarNivel())
+
 				estado = FIN;
+
 
 		}
 		if (mundo.getVidas() < 1)
+
 			estado = GAMEOVER;
+
 	}
+
 }
